73da08891a4d79155da87493c3d1dfdb377148ad
justinberg <justinmberg97@gmail.com>
2025-06-16 16:15:56 -0500

    added daniel changes to my files

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const { Sequelize, DataTypes, Model } = require("sequelize");
 // These match the environment variables set in docker-compose.yml for the 'backend' service
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "root"; // This will be 'root' when run via Docker Compose
+const DB_PASSWORD = process.env.DB_PASSWORD || ""; // This will be 'root' when run via Docker Compose
 const DB_NAME = process.env.DB_NAME || "coffee_db";
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
