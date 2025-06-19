daab5b3c4420fa7a261c6af9ebb1b7a1c4d334c6
justinberg <justinmberg97@gmail.com>
2025-06-19 15:58:45 -0500

    fixed DB Connection

--- server/db.js
+++ server/db.js
@@ -1,37 +1,18 @@
-const { Sequelize, DataTypes, Model } = require("sequelize");
+// const { Sequelize, DataTypes, Model } = require("sequelize");
 
-// Use Railway's MYSQL_URL if provided, OTHERWISE fallback to the local values
-const DB_URL = process.env.MYSQL_URL || "mysql://root:root@localhost:3306/coffee_db";
+// // Use Railway's MYSQL_URL if provided, OTHERWISE fallback to the local values
+// const MYSQL_URL = process.env.MYSQL_URL || "mysql://root:root@localhost:3306/coffee_db";
 
-const db = new Sequelize(DB_URL, {
-  dialect: "mysql",
-  logging: false,
-});
-console.log("MYSQL_URL =", process.env.MYSQL_URL);
-
-
-module.exports = { db, DataTypes, Model };
+// const db = new Sequelize(DB_URL, {
+//   dialect: "mysql",
+//   logging: false,
+// });
+// console.log("MYSQL_URL =", process.env.MYSQL_URL);
 
 
+// module.exports = { db, DataTypes, Model };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // server/db.js
+// server/db.js
 // const { Sequelize, DataTypes, Model } = require("sequelize");
 
 // // Get DB connection details from environment variables
@@ -48,3 +29,33 @@ module.exports = { db, DataTypes, Model };
 // });
 
 // module.exports = { db, DataTypes, Model };
+
+
+// server/db.js
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+let db;
+
+if (process.env.MYSQL_URL) {
+  // Railway: use full connection string
+  console.log("Using Railway MYSQL_URL:", process.env.MYSQL_URL);
+  db = new Sequelize(process.env.MYSQL_URL, {
+    dialect: "mysql",
+    logging: false,
+  });
+} else {
+  // Local or Docker Compose environment
+  const DB_HOST = process.env.DB_HOST || "localhost";
+  const DB_USER = process.env.DB_USER || "root";
+  const DB_PASSWORD = process.env.DB_PASSWORD || "";
+  const DB_NAME = process.env.DB_NAME || "coffee_db";
+
+  console.log("Using Local DB settings:", DB_HOST, DB_NAME);
+  db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  });
+}
+
+module.exports = { db, DataTypes, Model };
