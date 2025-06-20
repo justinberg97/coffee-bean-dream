d4f36e89807deff02cc7370e1127c31fbc64830e
justinberg <justinmberg97@gmail.com>
2025-06-20 11:34:26 -0500

    updated app js for deployment

--- server/app.js
+++ server/app.js
@@ -10,10 +10,19 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../dist")));
-
+// API routes
 app.use("/api", require("./routes"));
 
+// ✅ Serve Angular static build
+const angularDistPath = path.join(__dirname, "../public/coffee-bean-dream-public/dist/coffee-bean-dream-public");
+app.use(express.static(angularDistPath));
+
+// ✅ Fallback for Angular routing (must go after static + /api routes)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(angularDistPath, "index.html"));
+});
+
+// ❌ Keep this 404 handler LAST
 app.use((req, res) => {
   res.status(404).send({
     error: "404 - Not Found",
@@ -33,3 +42,45 @@ app.use((error, req, res, next) => {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const morgan = require("morgan");
+// const path = require("path");
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(morgan("dev"));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, "../dist")));
+
+// app.use("/api", require("./routes"));
+
+// app.use((req, res) => {
+//   res.status(404).send({
+//     error: "404 - Not Found",
+//     message: "No route found for the requested URL",
+//   });
+// });
+
+// app.use((error, req, res, next) => {
+//   console.error("SERVER ERROR: ", error);
+//   if (res.statusCode < 400) res.status(500);
+//   res.send({
+//     error: error.message,
+//     name: error.name,
+//     message: error.message,
+//     table: error.table,
+//   });
+// });
+
+// module.exports = app;
